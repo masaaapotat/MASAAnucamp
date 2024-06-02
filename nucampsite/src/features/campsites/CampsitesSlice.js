221e7adec465053a6144646b76fb6d7250492a30
masaapotat <masaakavulanya@gmail.com>
2024-06-02 14:05:48 +0300

    added the missing dependncies to make it work

--- nucampsite/src/features/campsites/CampsitesSlice.js
+++ nucampsite/src/features/campsites/CampsitesSlice.js
@@ -1,17 +1,15 @@
-// because it doesnt export that is why we're using camel case
-import { CAMPSITES } from "../../app/shared/CAMPSITES";
+import { CAMPSITES } from '../../app/shared/CAMPSITES';
 
-// selector functions that we can use to select specific pieces of data
-// selectAllCampsites use by our campsite list componnent to access the campsotes array
 export const selectAllCampsites = () => {
-  return CAMPSITES;
-};
-// selectRandomCampsite
-// export const selectRandomCampsite = () => {
-//   return CAMPSITES[Math.floor(CAMPSITES.length * Math.random())];
-// };
+    return CAMPSITES;
+}
 
+// export const selectRandomCampsite = () => {
+//     // wrong tessy return Math.floor(Math.random() * selectAllCampsites.length);
+//     return CAMPSITES[Math.floor(CAMPSITES.length * Math.random())];
+// }
+
+//function takes an argument of id and returns the first object in the CAMPSITES array with a matching id
 export const selectCampsiteById = (id) => {
-  // what we want to find is the first campsite in the campsites array with an id === the id that was passed in
-  return CAMPSITES.find((campsite) => campsite.id === id);
-};
+    return CAMPSITES.find((campsite) => campsite.id === id);
+}
--- nucampsite/src/pages/CampsitesDirectoryPage.js
+++ nucampsite/src/pages/CampsitesDirectoryPage.js
@@ -1,24 +1,20 @@
-import { useState } from 'react';
-import { Container, Row, Col } from 'reactstrap';
-import CampsiteDetail from '../features/campsites/CampsiteDetail';
-import CampsitesList from '../features/campsites/CampsitesList';
-import { selectCampsiteById } from '../features/campsites/CampsitesSlice';
+import { useState } from "react";
+import { Container, Row, Col } from "reactstrap";
+import CampsiteDetail from "../features/campsites/CampsiteDetail";
+import CampsitesList from "../features/campsites/CampsitesList";
+import { selectCampsiteById } from "../features/campsites/CampsitesSlice";
 
 const CampsitesDirectoryPage = () => {
-  // State to store the ID of the selected campsite
   const [campsiteId, setCampsiteId] = useState(0);
-
-  // Get the actual campsite object based on the ID
   const selectedCampsite = selectCampsiteById(campsiteId);
- 
+
   return (
     <Container>
-      {/* Removed the commented-out button section */}
       <Row>
-        <Col sm='5' md='7'>
-          <CampsitesList setCampsiteId={setCampsiteId} /> {/* Pass setCampsiteId prop */}
+        <Col sm="5" md="7">
+          <CampsitesList setCampsiteId={setCampsiteId} />
         </Col>
-        <Col sm='7' md='5'>
+        <Col sm="7" md="5">
           <CampsiteDetail campsite={selectedCampsite} />
         </Col>
       </Row>
